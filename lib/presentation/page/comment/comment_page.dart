25b73c73039aab1f76fa740f9a421205c2196520
hyun471 <khs101ttl@gmail.com>
2025-08-28 21:13:07 +0900

    🚧 comment page UI 작업중

---
+++ lib/presentation/page/comment/comment_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CommentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Divider(
+            color: Colors.black,
+            thickness: 1,
+          ),
+          Row(),
+        ],
+      ),
+    );
+  }
+}
