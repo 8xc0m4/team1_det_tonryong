1f33fdb9913b447f3cc260ca8e158c51db697746
wackyturtle <wcytutl01@gmail.com>
2025-08-29 21:46:57 +0900

    ✨

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:team1_det_tonryong/presentation/page/detail/detail_page.dart';
 import 'package:team1_det_tonryong/presentation/page/login/login_page.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: DetailPage(),
     );
   }
 }
